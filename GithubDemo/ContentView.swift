c63d1871c581a97088e48736b954cb61ac1cbbe1
hessah11h <hussah.alqusi@gmail.com>
2024-09-26 16:57:08 +0300

    Initial Commit

---
+++ GithubDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubDemo
+//
+//  Created by hussah alqusi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
